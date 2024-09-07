0101edd342307490eec4aabb19205766e13804e9
JesusFilth <Adrian_McArrow@mail.ru>
2024-09-07 19:22:24 +0300

    Add behavior enemys, combat, camera moving

--- Assets/_Sourse_/Scripts/Camera/FollowCameraToPlayerX.cs
+++ Assets/_Sourse_/Scripts/Camera/FollowCameraToPlayerX.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class FallowToPlayerX : MonoBehaviour
+public class FollowCameraToPlayerX : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _offsetY = 2.0f;
@@ -9,6 +9,7 @@ public class FallowToPlayerX : MonoBehaviour
     [SerializeField] private float _smoothSpeed = 0.125f;
 
     private Transform _transform;
+    private bool _isActive = true;
 
     private void Awake()
     {
@@ -23,8 +24,15 @@ public class FallowToPlayerX : MonoBehaviour
 
     private void Update()
     {
+        if (_isActive == false)
+            return;
+
         Vector3 desiredPosition = new Vector3(_target.position.x + _offsetX, _target.position.y + _offsetY, _transform.position.z);
 
         _transform.position = Vector3.Lerp(_transform.position, desiredPosition, _smoothSpeed * Time.deltaTime);
     }
+
+    public void On() => _isActive = true;
+
+    public void Off() =>_isActive = false;
 }
