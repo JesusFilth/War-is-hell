b172446a3a18a6c7e9032e4a73d8aa199d023e44
VladislavGerasimovich <vladgerasimovich@icloud.com>
2024-09-11 19:57:12 +0300

    equipBat

---
+++ Assets/Kevin Iglesias/Caster Mage Animations/Scripts/AddCastEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace KevinIglesias
+{
+
+    public class AddCastEffect : StateMachineBehaviour
+    {
+
+        CastSpells cS;
+
+        public CastHand castHand;
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+
+            if (cS == null)
+            {
+                cS = animator.GetComponent<CastSpells>();
+            }
+
+            if (cS != null)
+            {
+                cS.SpawnEffect(castHand);
+            }
+        }
+    }
+}
---
+++ Assets/Kevin Iglesias/Caster Mage Animations/Scripts/ThrowHealing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace KevinIglesias
+{
+
+    public class ThrowHealing : StateMachineBehaviour
+    {
+
+        CastSpells cS;
+
+        public CastHand castHand;
+
+        public float spawnDelay;
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+
+            if (cS == null)
+            {
+                cS = animator.GetComponent<CastSpells>();
+            }
+
+            if (cS != null)
+            {
+                cS.ThrowHealing(castHand, spawnDelay);
+            }
+        }
+    }
+}
