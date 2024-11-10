5f564b8edab750726eff680019174e517e2e78d7
JesusFilth <Adrian_McArrow@mail.ru>
2024-11-10 18:52:59 +0300

    Sesected ui imagef for release

---
+++ Assets/_Sourse_/Scripts/Core/Localization/LocalizationInitialize.cs
@@ -0,0 +1,33 @@
+using Lean.Localization;
+using UnityEngine;
+using GamePush;
+
+public class LocalizationInitialize : MonoBehaviour
+{
+    private const string EnglishCode = "English";
+    private const string RussinCode = "Russian";
+    private const string TurkishCode = "Turkish";
+
+    [SerializeField] private LeanLocalization _leanLocalization;
+
+    private void ChangeLenguage()
+    {
+        Language language = GP_Language.Current();
+
+        switch (language)
+        {
+            case Language.English:
+                _leanLocalization.SetCurrentLanguage(EnglishCode);
+                break;
+            case Language.Russian:
+                _leanLocalization.SetCurrentLanguage(RussinCode);
+                break;
+            case Language.Turkish:
+                _leanLocalization.SetCurrentLanguage(TurkishCode);
+                break;
+            default:
+                _leanLocalization.SetCurrentLanguage(EnglishCode);
+                break;
+        }
+    }
+}
