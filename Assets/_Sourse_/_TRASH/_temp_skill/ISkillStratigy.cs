b9156506faa02baefaf79b5671595154a19a5c4e
JesusFilth <Adrian_McArrow@mail.ru>
2024-11-19 12:30:51 +0300

    Remove trash folders

--- Assets/_Sourse_/_TRASH/_temp_skill/ISkillStratigy.cs
+++
@@ -1,9 +0,0 @@
-using Sourse.Scripts.Characters.Player;
-using Sourse.Scripts.Skills;
-
-public interface ISkillStratigy
-{
-    void Execute(PlayerAbilitys abilitys, StatParameterModel statModel);
-
-    void Execute(PlayerAbilitys abilitys, SkillActive skill);
-}
